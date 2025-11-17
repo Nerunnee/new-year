2db1fa312d562a4a27a8e66bbebf452a2ba40971
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-11-17 20:37:51 +0800

    HOMEWORK

--- index.js
+++ index.js
@@ -1,6 +1,87 @@
-//1) triangle sides
-let a = 3;
+//1
+let square1 = 5;
+let perimeter1 = square1 * 4;
+console.log(`1) The perimeter is ${perimeter1}`);
+//2
+let square2 = 8;
+let talbai2 = square2 ** 2;
+console.log(`2) The square is ${talbai2}`);
+//3, 4, 5
+let side3 = 4;
+let side3_2 = 6;
+let perimeter3 = (side3 + side3_2) * 2;
+let square4 = side3 * side3_2;
+let haritsaa = square4 / perimeter3;
+console.log(`3) The perimeter is ${perimeter3}`);
+console.log(`4) The square is ${square4}`);
+console.log(`5) The ratio is ${haritsaa} `);
+//6, 7
+let radius6 = 3;
+let perimeter6 = 2 * radius6 * 3.14;
+let square6 = 3.14 * radius6 ** 2;
+console.log(`6) The perimeter is ${perimeter6}`);
+console.log(`7) The square is ${square6}`);
+//8
+let radius8 = 3;
+let perimeter8 = 2 * radius8 * 3.14;
+let square8 = 3.14 * radius8 ** 2;
+let haritsaa2 = square8 / perimeter8;
+console.log(`8) The ratio is ${haritsaa2}`);
+//9
+let radius9_1 = 3;
+let radius9_2 = 7;
+let square9_1 = radius9_1 ** 2 * 3.14;
+let square9_2 = radius9_2 ** 2 * 3.14;
+let ylgawar9 = radius9_2 - radius9_1;
+console.log(`9) The difference is ${ylgawar9}`);
+//10
+let radius10 = 10;
+let diameter10 = radius10 * 2;
+console.log(`10) The diameter is ${diameter10}`);
+//11
+let t11 = 25;
+let time11 = t11 % 12;
+console.log(`11) The time is ${time11} am or pm`);
+//12
+//Oilgoogui
+//13
+let x = 8.7;
+let y = 7;
+let hariu = x % y;
+console.log(`13) ${hariu}`);
+//14
+let a = 15;
 let b = 4;
-let c = 5;
-// triangle perimeter=
-console.log(a + b + c);
+let hariu2 = a % b;
+console.log(`14) The result is ${hariu2}`);
+//15
+let c = 123;
+let hariu3 = c % 10;
+console.log(`15) The result is ${hariu3}`);
+//16
+let a1 = 3;
+let a2 = 7;
+let togtol = a2 - a1;
+let a3 = a2 + togtol;
+console.log(`16) The next number is ${a3}`);
+//17
+let b2 = 4;
+let b3 = 6;
+let togtol2 = b3 - b2;
+let b4 = b3 + togtol2;
+let b5 = b4 + togtol2;
+console.log(`17) The next 2 number is ${b4} and ${b5}`);
+//18
+let x2 = 10;
+let y2 = 20;
+let hariu4 = x2 + y2 * 2;
+console.log(`18) The result is ${hariu4}`);
+//19
+let x3 = 5;
+let y3 = 10;
+let hariu5 = (x3 + y3) * 2;
+console.log(`19) The result is ${hariu5}`);
+//20
+let last = 7;
+let shalgah = last % 2;
+console.log(`20) It is a odd number because its remaining is ${shalgah}`);
