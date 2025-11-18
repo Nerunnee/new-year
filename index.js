b94e30db6d0d612a22200569cc707f6a909e935f
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-11-18 21:09:26 +0800

    Buren hiigeegui

--- index.js
+++ index.js
@@ -1,87 +1,58 @@
-//1
-let square1 = 5;
-let perimeter1 = square1 * 4;
-console.log(`1) The perimeter is ${perimeter1}`);
-//2
-let square2 = 8;
-let talbai2 = square2 ** 2;
-console.log(`2) The square is ${talbai2}`);
-//3, 4, 5
-let side3 = 4;
-let side3_2 = 6;
-let perimeter3 = (side3 + side3_2) * 2;
-let square4 = side3 * side3_2;
-let haritsaa = square4 / perimeter3;
-console.log(`3) The perimeter is ${perimeter3}`);
-console.log(`4) The square is ${square4}`);
-console.log(`5) The ratio is ${haritsaa} `);
-//6, 7
-let radius6 = 3;
-let perimeter6 = 2 * radius6 * 3.14;
-let square6 = 3.14 * radius6 ** 2;
-console.log(`6) The perimeter is ${perimeter6}`);
-console.log(`7) The square is ${square6}`);
-//8
-let radius8 = 3;
-let perimeter8 = 2 * radius8 * 3.14;
-let square8 = 3.14 * radius8 ** 2;
-let haritsaa2 = square8 / perimeter8;
-console.log(`8) The ratio is ${haritsaa2}`);
-//9
-let radius9_1 = 3;
-let radius9_2 = 7;
-let square9_1 = radius9_1 ** 2 * 3.14;
-let square9_2 = radius9_2 ** 2 * 3.14;
-let ylgawar9 = radius9_2 - radius9_1;
-console.log(`9) The difference is ${ylgawar9}`);
-//10
-let radius10 = 10;
-let diameter10 = radius10 * 2;
-console.log(`10) The diameter is ${diameter10}`);
-//11
-let t11 = 25;
-let time11 = t11 % 12;
-console.log(`11) The time is ${time11} am or pm`);
-//12
-//Oilgoogui
-//13
-let x = 8.7;
-let y = 7;
-let hariu = x % y;
-console.log(`13) ${hariu}`);
 //14
-let a = 15;
-let b = 4;
-let hariu2 = a % b;
-console.log(`14) The result is ${hariu2}`);
+const number14 = -10;
+if (number14 > 100) {
+  console.log(`14) The number is greater than 100.`);
+} else if (number14 < 0) {
+  console.log(`14) The number is negative.`);
+} else {
+  console.log(`14) The number is greater than 0 but lesser than 100.`);
+}
 //15
-let c = 123;
-let hariu3 = c % 10;
-console.log(`15) The result is ${hariu3}`);
+const number15 = 1738;
+const number15_10 = number15 / 10;
+const number15_20 = number15_10 * 2;
+const number15_30 = number15_10 * 3;
+console.log(`15) The 10 percent of your number is ${number15_10}`);
+console.log(`15) The 20 percent of your number is ${number15_20}`);
+console.log(`15) The 30 percent of your number is ${number15_30}`);
 //16
-let a1 = 3;
-let a2 = 7;
-let togtol = a2 - a1;
-let a3 = a2 + togtol;
-console.log(`16) The next number is ${a3}`);
+let number16 = -2;
+if (number16 > 100) {
+  number16 = 100;
+  console.log(`16) ${number16}`);
+} else {
+  console.log(`16) ${number16}`);
+}
 //17
-let b2 = 4;
-let b3 = 6;
-let togtol2 = b3 - b2;
-let b4 = b3 + togtol2;
-let b5 = b4 + togtol2;
-console.log(`17) The next 2 number is ${b4} and ${b5}`);
+const str17_1 = "mongolia";
+const str17_2 = "mongolia";
+console.log(`17) ${str17_1 === str17_2}`);
 //18
-let x2 = 10;
-let y2 = 20;
-let hariu4 = x2 + y2 * 2;
-console.log(`18) The result is ${hariu4}`);
+const number18 = 98;
+const number18_2 = (number18 % 10) * 10;
+const number18_3 = (number18 - (number18 % 10)) / 10;
+const number18_4 = number18_3 + number18_2;
+if (number18 > 9 && number18 < 100) {
+  console.log(`18) The reverted number is ${number18_4}`);
+} else {
+  console.log(`18) Whoops!!! Only enter number with 2 digits.`);
+}
 //19
-let x3 = 5;
-let y3 = 10;
-let hariu5 = (x3 + y3) * 2;
-console.log(`19) The result is ${hariu5}`);
-//20
-let last = 7;
-let shalgah = last % 2;
-console.log(`20) It is a odd number because its remaining is ${shalgah}`);
+let number19 = 0;
+if (number19 < 0) {
+  number19 = number19 * -1;
+  console.log(`19) ${number19}`);
+} else if (number19 === 0) {
+  console.log(`19) Your number is zero.`);
+} else {
+  console.log(`19) ${number19}`);
+}
+//20 Zalhuu hursen. Buren oilgoogui
+//21 buren hiigeegui.
+const number21 = 7894;
+const numberOddEven = number21 % 2;
+if (numberOddEven === 0) {
+  console.log(`20) You are allowed to go in tuesday, thursday, saturday`);
+} else {
+  console.log(`20) You are allowed to go in monday, wednesday, friday`);
+}
